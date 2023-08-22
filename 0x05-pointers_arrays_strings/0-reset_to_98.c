13de8d700497b81d371a6baf87b55fd50e10b7ac
ClintonNwokocha <leoclinton2011@hotmail.com>
2023-08-22 06:19:39 -0700

    Correct comment to meet betty 80 line style

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * reset_to_98  - takes a pointer to an int and updates the value to point to 98
+ * reset_to_98  - takes a pointer to an int and 
+ * updates the value to point to 98
  * @n: input
  * Return: n
  */
