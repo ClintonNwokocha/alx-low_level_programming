acb24962f2f81da99d46688afefeca7a5afaab27
Clinton Nwokocha <leoclinton2011@hotmail.com>
2023-10-26 22:02:49 -0700

    Implemented factorial function

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,19 +1,18 @@
 #include "main.h"
 
 /**
- * factorial - Computes the factorial of a given number.
- * @n: The number to compute the factorial of.
- *
- * Return: The factorial of n.
- *         If n is lower than 0, the function returns -1 to indicate an error.
- *         Factorial of 0 is 1.
- */
+  * factorial - function parameter
+  *
+  * @n: function parameter
+  *
+  * Return: Function n.
+  */
 int factorial(int n)
 {
-    if (n < 0)
-        return (-1);
-    if (n == 0)
-        return (1);
-    return (n * factorial(n - 1));
+	if (n < 0)
+	return (-1);
+	if (n == 0)
+	return (1);
+	return (n * factorial(n - 1));
 }
 
