f96b6de53f387b65dbc8e801875b3d7af867390f
Clinton Nwokocha <leoclinton2011@hotmail.com>
2023-10-26 21:51:02 -0700

    Fixed Betty style and documentation issues

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,16 +1,19 @@
 #include "main.h"
-/**
- * function_name - Short description of the function's purpose
- * @param_name: Description of parameter
- *
- * Return: Description of return value, if any
- */
 
+/**
+  * _print_rev_recursion - main function
+  *
+  * @s: Function parameter
+  *
+  * Return: Voidu
+  */
 void _print_rev_recursion(char *s)
 {
-    if (*s == '\0')
-        return;
-    _print_rev_recursion(s + 1);
-    _putchar(*s);
+	if (*s)
+	{
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+	}
 }
+
 
