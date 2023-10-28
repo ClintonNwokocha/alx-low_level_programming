6c847a54ef017ad594caaf8517fcc0141414e660
Clinton Nwokocha <leoclinton2011@hotmail.com>
2023-10-28 05:52:04 -0700

    new files

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - returns the lenght of the string
+ * @s: string
+ * Return: lenght
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	return (length);
+}
