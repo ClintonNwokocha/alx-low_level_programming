a0cc5ea1062bc0f16af94972a06ac6af9e4f60bb
Clinton Nwokocha <leoclinton2011@hotmail.com>
2023-11-03 18:28:20 -0700

    added reaclloc file

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stddef.h>
+#include <stdlib.h>
 /* Function prototypes */
 
 void *malloc_checked(unsigned int b);
@@ -8,4 +9,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 int _putchar(char c);
+void simple_print_buffer(char *buffer, unsigned int size);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 #endif
