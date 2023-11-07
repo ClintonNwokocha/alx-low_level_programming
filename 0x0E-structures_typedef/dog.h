90505a332713d2b50047b57a5211c256c143597e
Clinton Nwokocha <leoclinton2011@hotmail.com>
2023-11-07 00:43:00 -0800

    Added dog.h file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure for a dog with a name, age, and owner
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Description: A struct to store infomration about the dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
